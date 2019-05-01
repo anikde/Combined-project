ae6444e8d59073838a85b1067fc65fb559e5c6e4
Sumit Dey <dey.sumit.kr@gmail.com>
2019-05-01 14:31:26 +0200

    minor formatting changes

--- basics.cpp
+++ basics.cpp
@@ -1,13 +1,11 @@
 #include "basics.h"
 
 float addByValue(float x, float y){
-    float z; 
-    z=x+y;
-    return z;
+	float z; 
+	z=x+y;
+	return z;
 }
 
 void addByReference(float x, float y, float * const result){
-   
-    *result = x + y ;
-    
+	*result = x + y ;
 }	
